6e4adebe67bf29e99e7c154f793af5f97eb1f417
eclectic_mind <52823445+eclectic-mind@users.noreply.github.com>
2020-09-14 11:39:45 +0300

    feat: sorting by "for" cycle

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let result = [];
+    if (towelSort.arguments.length === 0 || matrix.length === 0) {
+        return [];
+    }
+    const simplyAdd = (res, element) => {
+        res.push(element);
+    };
+    const addReversed = (res, element) => {
+        res.push(element.reverse());
+    };
+    for (let i = 0; i < matrix.length; i += 1) {
+        if (i % 2 === 0) {
+            simplyAdd(result, matrix[i]);
+        } else {
+            addReversed(result, matrix[i]);
+        }
+    }
+    console.log(result);
+    return result.flat();
+};
